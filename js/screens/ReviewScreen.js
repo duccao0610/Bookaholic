b894e9941c7f491b8d9bb9e98491376d255e094b
duccao0610 <caominhduc610@gmail.com>
2021-03-30 08:03:48 +0700

    view book detail -Duc

--- js/screens/ReviewScreen.js
+++ js/screens/ReviewScreen.js
@@ -2,8 +2,21 @@ import { book } from "../models/book.js";
 
 const $template = document.createElement('template');
 $template.innerHTML = /*html*/`
+    <style>
+        #reviews{
+            display :flex;
+            flex-direction : column;
+            justify-content : space-between;
+            margin : auto;
+            width : 50%;
+            margin-top : 10vh;
+        }
+    </style>
     <my-header></my-header>
     <book-info-wrapper id="book-info"></book-info-wrapper>
+    <div id="reviews">
+
+    </div>
     <my-footer></my-footer>
 `;
 
@@ -14,6 +27,7 @@ export default class ReviewScreen extends HTMLElement {
         this.shadowRoot.appendChild($template.content.cloneNode(true));
 
         this.$bookInfo = this.shadowRoot.getElementById("book-info");
+        this.$reviewList = this.shadowRoot.getElementById("reviews");
 
     }
 
@@ -22,8 +36,16 @@ export default class ReviewScreen extends HTMLElement {
         this.$bookInfo.setAttribute("author", book.author);
         this.$bookInfo.setAttribute("intro", book.intro);
         this.$bookInfo.setAttribute("book-cover", book.cover_img);
-    }
 
+        let reviews = book.reviews;
+        for (let review of reviews) {
+            let $review = document.createElement("review-wrapper");
+            $review.setAttribute("comment", review.comment);
+            $review.setAttribute("rating", review.rating);
+            $review.setAttribute("username", review.username);
+            this.$reviewList.appendChild($review);
+        }
+    }
 
 
 }
